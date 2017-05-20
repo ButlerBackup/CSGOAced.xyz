cdcca70d22db9fe38969794f8387442cb9266971
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-20 19:12:31 +0100

    Add CoinFlip Image

--- lib/view/coinflip.php
+++ lib/view/coinflip.php
@@ -2,7 +2,7 @@
 	<div class="col-xs-6 col-md-3">
 		<div class="thumbnail">
 			<div class="coin-flip-cont">
-				<div class="coin"><div class="front" style="background: url(http://localhost/CSGOAced/img/avatar2.jpg); background-size: 100%;"></div>
+				<div class="coin"><div class="front" style="background: url(<?php echo $Link->Website; ?>img/avatar2.jpg); background-size: 100%;"></div>
 			</div></div><div class="caption">
 				<h4>X Coins</h4>
 				<button type="button" class="btn btn-info btn-md PlaceBet">Place Bet <span class="glyphicon glyphicon-fire"></span></button>
