d2df8738ddd0374e54e94ef46f269b9c6d9fb47e
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-22 12:01:52 +0100

    Changed SkinPrices Update Method

--- lib/database/UpdateSkinPrices.php
+++ lib/database/UpdateSkinPrices.php
@@ -3,19 +3,24 @@
 $JSONpricelist = file_get_contents("https://api.csgofast.com/price/all");
 $Pricelist = json_decode($JSONpricelist);
 
-$sth = $conn->prepare( "IF EXISTS(SELECT MarketName FROM Skins WHERE MarketName=:MarketName)
+
+$sth = $conn->prepare("DELETE FROM SkinPrices WHERE '1'='1';");
+$sth->execute();
+
+$sth = $conn->prepare( "IF NOT EXISTS(SELECT MarketName FROM Skins WHERE MarketName=:MarketName)
 						THEN
-							UPDATE SkinPrices SET BuyPrice=:BuyPrice, SellPrice=:SellPrice WHERE (SkinMarketName=:MarketName);
-						ELSE
 							INSERT INTO Skins (MarketName) VALUES (:MarketName);
-							INSERT INTO SkinPrices (SkinMarketName, BuyPrice, SellPrice) VALUES (:MarketName, :BuyPrice, :SellPrice);
 						END IF;");
-$sth->bindParam(':BuyPrice', $buyPrice);
-$sth->bindParam(':SellPrice', $sellPrice);
 $sth->bindParam(':MarketName', $market_name);
+
+$sth1 = $conn->prepare("INSERT INTO SkinPrices (SkinMarketName, BuyPrice, SellPrice) VALUES (:MarketName, :BuyPrice, :SellPrice);");
+$sth1->bindParam(':BuyPrice', $buyPrice);
+$sth1->bindParam(':SellPrice', $sellPrice);
+$sth1->bindParam(':MarketName', $market_name);
 
 foreach($Pricelist AS $market_name => $price) {
 	$buyPrice = $price * 100 - 3;
 	$sellPrice = $price * 107 + 3;
 	$sth->execute();
+	$sth1->execute();
 }
