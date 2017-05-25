ca81086b5718672d4836aa6fea20321c3fc1dcbc
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-25 15:32:20 +0100

    Fix Script.php Bug

--- lib/view/scripts.php
+++ lib/view/scripts.php
@@ -4,8 +4,26 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+<!-- CSGOAced.xyz -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.1/socket.io.js"></script>
+
+<script type="text/javascript"> 
+	var User = function(name, avatar){
+		this.name = name;
+		this.avatar = avatar;
+	}
+
+	function GetUser(){
+		return (new User("<?php echo (isset($steamprofile) ? $steamprofile['personaname'] : ""); ?>", "<?php echo (isset($steamprofile) ? $steamprofile['avatarfull'] : ""); ?>"));
+	}
+
+	function GetServer(){ 
+		return "<?php echo $Link->NodePath; ?>"; 
+	} 
+</script>
+
 <!-- Socket.io -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.1/socket.io.js"></script>
 
 <!-- CSGOAced.xyz -->
-<script type="text/javascript" src="<?php echo $Link->Website . 'js/chat.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $Link->Website . 'js/master.js'; ?>"></script>
