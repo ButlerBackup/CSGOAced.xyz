72276c6726e49bfbc396165e78a3c600bc852117
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-05-29 15:38:05 +0100

    Footer Links Open in New Tab

--- lib/view/footer.php
+++ lib/view/footer.php
@@ -1,12 +1,12 @@
 <footer class="footer">
 	<div class="container">
 		<p class="text-muted"><span class="glyphicon glyphicon-copyright-mark"></span> <a href="<?php echo $Link->Website; ?>">CSGOAced.xyz</a> All Rights Reserved. 
-		<a href="<?php echo $Link->Facebook; ?>"><i class="fa fa-facebook-square fa-2x text-muted" aria-hidden="true" ></i></a>
-		<a href="<?php echo $Link->Twitter; ?>"><i class="fa fa-twitter-square fa-2x text-muted"></i></a>
-		<a href="<?php echo $Link->SteamGroup; ?>"><i class="fa fa-steam fa-2x text-muted"></i></a>
-		<a href="<?php echo $Link->Instagram; ?>"><i class="fa fa-instagram fa-2x text-muted"></i></a>
-		<a href="<?php echo $Link->GitHub; ?>"><i class="fa fa-github fa-2x text-muted"></i></a>
-		<a href="mailto:<?php echo $Link->Mail; ?>"><i class="fa fa-envelope-square fa-2x text-muted"></i></a>
+		<a href="<?php echo $Link->Facebook; ?>" target="_blank"><i class="fa fa-facebook-square fa-2x text-muted" aria-hidden="true" ></i></a>
+		<a href="<?php echo $Link->Twitter; ?>" target="_blank"><i class="fa fa-twitter-square fa-2x text-muted"></i></a>
+		<a href="<?php echo $Link->SteamGroup; ?>" target="_blank"><i class="fa fa-steam fa-2x text-muted"></i></a>
+		<a href="<?php echo $Link->Instagram; ?>" target="_blank"><i class="fa fa-instagram fa-2x text-muted"></i></a>
+		<a href="<?php echo $Link->GitHub; ?>" target="_blank"><i class="fa fa-github fa-2x text-muted"></i></a>
+		<a href="mailto:<?php echo $Link->Mail; ?>" target="_blank"><i class="fa fa-envelope-square fa-2x text-muted"></i></a>
 		</p>
 	</div>
 </footer>
