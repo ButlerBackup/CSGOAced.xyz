1e7be5338201625250bc5f3bd7351caac054985e
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-05 15:03:41 +0100

    Update Steam Icon

--- lib/view/footer.php
+++ lib/view/footer.php
@@ -5,7 +5,7 @@
 			<p class="text-muted"><span class="glyphicon glyphicon-copyright-mark"></span> <a href="<?php echo $Link->Website; ?>">CSGOAced.xyz</a> All Rights Reserved. 
 			<a href="<?php echo $Link->Facebook; ?>" target="_blank"><i class="fa fa-facebook-square fa-2x text-muted" aria-hidden="true" ></i></a>
 			<a href="<?php echo $Link->Twitter; ?>" target="_blank"><i class="fa fa-twitter-square fa-2x text-muted"></i></a>
-			<a href="<?php echo $Link->SteamGroup; ?>" target="_blank"><i class="fa fa-steam fa-2x text-muted"></i></a>
+			<a href="<?php echo $Link->SteamGroup; ?>" target="_blank"><i class="fa fa-steam-square fa-2x text-muted"></i></a>
 			<a href="<?php echo $Link->Instagram; ?>" target="_blank"><i class="fa fa-instagram fa-2x text-muted"></i></a>
 			<a href="<?php echo $Link->GitHub; ?>" target="_blank"><i class="fa fa-github fa-2x text-muted"></i></a>
 			<a href="<?php echo $Link->Discord; ?>" target="_blank"><i class="fa fa-life-ring fa-2x text-muted"></i></a>
