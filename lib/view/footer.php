051d2e5a7949187fbb8eac164987cef11708bb76
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-05 17:26:39 +0100

    Add Issues Icon to Footer

--- lib/view/footer.php
+++ lib/view/footer.php
@@ -8,6 +8,7 @@
 			<a href="<?php echo $Link->SteamGroup; ?>" target="_blank"><i class="fa fa-steam-square fa-2x text-muted"></i></a>
 			<a href="<?php echo $Link->Instagram; ?>" target="_blank"><i class="fa fa-instagram fa-2x text-muted"></i></a>
 			<a href="<?php echo $Link->GitHub; ?>" target="_blank"><i class="fa fa-github fa-2x text-muted"></i></a>
+			<a href="<?php echo $Link->Issues; ?>" target="_blank"><i class="fa fa-bug fa-2x text-muted"></i></a>
 			<a href="<?php echo $Link->Discord; ?>" target="_blank"><i class="fa fa-life-ring fa-2x text-muted"></i></a>
 			<a href="mailto:<?php echo $Link->Mail; ?>" target="_blank"><i class="fa fa-envelope-square fa-2x text-muted"></i></a>
 			</p>
