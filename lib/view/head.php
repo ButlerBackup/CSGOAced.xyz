522409c7f95caaf570e388cf77132b3c05985b96
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-06 14:03:25 +0100

    Remove Bootstrap Integrity

--- lib/view/head.php
+++ lib/view/head.php
@@ -40,7 +40,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
 
 <!-- Bootstrap -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 <!-- Stylesheet -->
 <link rel="stylesheet" href="<?php echo $Link->Website . "css/styles.css" ;?>">
