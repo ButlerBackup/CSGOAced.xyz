1976cccc25a73599d21d80b856d737d5c462beab
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-09 14:34:47 +0100

    Remove Unecessary PHP

--- lib/view/head.php
+++ lib/view/head.php
@@ -46,4 +46,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.2.0/jquery-confirm.min.css">
 
 <!-- Stylesheet -->
-<link rel="stylesheet" href="<?php echo $Link->Website . "css/styles.css" ;?>">
+<link rel="stylesheet" href="css/styles.css">
