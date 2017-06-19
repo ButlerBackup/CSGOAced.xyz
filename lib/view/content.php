64f472a204eaf3858e33ffc98d3f8b67e98d77d9
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-19 08:09:03 +0100

    Add Release Countdown

--- lib/view/content.php
+++ lib/view/content.php
@@ -8,12 +8,33 @@
 		<div class="row">
 			<div class="alert alert-warning fade in">
 				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-				<span class="glyphicon glyphicon-time"></span> Officially Released on: <strong>24th June 2017!</strong>
+				<span class="glyphicon glyphicon-time"></span> Officially Released in: <strong id="countdown"></strong>
 			</div>
 			<div class="alert alert-warning fade in">
 				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 				<span class="glyphicon glyphicon-bullhorn"></span> Wanna be a <strong>beta tester</strong>? Contact us via our <a href="https://discord.gg/uzjBqxX"><strong>Discord Server</strong></a>! <span class="glyphicon glyphicon-thumbs-up"></span>
 			</div>
+			<script>
+				var countDownDate = new Date("Jun 23, 2017 00:00:00").getTime();
+
+				var x = setInterval(function() {
+					var now = new Date().getTime();
+
+					var distance = countDownDate - now;
+
+					var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+					var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+					document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+					if (distance < 0) {
+						clearInterval(x);
+						document.getElementById("countdown").innerHTML = "EXPIRED";
+					}
+				}, 1000);
+			</script>
 		</div>
 		<div class="row">
 			<?php
