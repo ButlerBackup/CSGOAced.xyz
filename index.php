03e4483e75ce23ca0f377e62e4d94ce1e40fb059
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-23 12:01:00 +0100

    Move Page Manager to Controller

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
 require_once ("lib/controller/CSGOAcedController.php");
-
-if (isset($_GET['m'])){
-	if (!isset($_SESSION['UID'])){
-		echo '<h3>Login to See This Content</h3>';
-		die();
-	}else{
-		if ($GetPage == $Page->Admin && isset($_SESSION['Role']) && $_SESSION['Role'] == "Admin"){
-			require_once ("lib/view/admin.php");
-		}elseif ($GetPage == $Page->TradeURL){
-			require_once ("lib/view/tradeurl.php");
-		}elseif ($GetPage == $Page->FreeCoins){
-			require_once ("lib/view/freecoins.php");
-		}elseif ($GetPage == $Page->Referal){
-			require_once ("lib/view/referal.php");
-		}elseif ($GetPage == $Page->Deposit){
-				require_once ('lib/database/ShowInventory.php');
-		}elseif ($GetPage == $Page->Withdraw){
-				require_once ('lib/database/ShowBotInventory.php');
-		}elseif ($GetPage == $Page->Emojis){
-			require_once ('lib/view/emojis.php');
-		}elseif (isset($_GET['updateinventory'])){
-			if (isset($_GET['bot'])){
-				require_once ('lib/database/UpdateBotInventory.php');
-			}else{
-				require_once ('lib/database/UpdateInventory.php');
-			}
-		}
-	}
-	die();
-}
-
 ?>
 
 <!DOCTYPE html>
