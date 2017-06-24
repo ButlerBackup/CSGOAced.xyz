ccb1ac45b2ab1e93ece43cc598280c4c1ddb47ec
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-24 21:26:32 +0100

    Fix Emoji Menu

--- lib/controller/CSGOAcedController.php
+++ lib/controller/CSGOAcedController.php
@@ -45,8 +45,8 @@ if (isset($_GET['m'])){
 				require_once ('lib/database/ShowInventory.php');
 		}elseif ($GetPage == $Page->Withdraw){
 				require_once ('lib/database/ShowBotInventory.php');
-		}elseif ($GetPage == $Page->Withdraw){
-				require_once ('lib/database/ShowBotInventory.php');
+		}elseif ($GetPage == $Page->Emojis){
+				require_once ('lib/view/emojis.php');
 		}elseif (isset($_GET['updateinventory'])){
 			if (isset($_GET['bot'])){
 				require_once ('lib/database/UpdateBotInventory.php');
