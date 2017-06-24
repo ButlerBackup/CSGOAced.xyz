054133a51b6285e2a9d1111c5c91db4697ccc242
TiagoSeverino <TiagoSeverino@users.noreply.github.com>
2017-06-24 16:42:10 +0100

    Remove Countdown and Giveaway

--- lib/view/content.php
+++ lib/view/content.php
@@ -6,35 +6,6 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="alert alert-info fade in">
-				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-				<span class="glyphicon glyphicon-time"></span> Officially Released in: <strong id="countdown"></strong>
-			</div>
-			<script>
-				var countDownDate = new Date("Jun 24, 2017 00:00:00").getTime();
-
-				var x = setInterval(function() {
-					var now = new Date().getTime();
-
-					var distance = countDownDate - now;
-
-					var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-					var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-					document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-
-					if (distance < 0) {
-						clearInterval(x);
-						document.getElementById("countdown").parentElement.classList.remove('alert-info');
-						document.getElementById("countdown").parentElement.classList.add('alert-success');
-						document.getElementById("countdown").parentElement.innerHTML = '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><span class="glyphicon glyphicon-gift"></span> Join Our <a href="https://gleam.io/i8OO9/csgoacedxyz-flip-knife-marble-fade-giveaway" target="_blank"><strong>Flip Knife | Marble Fade (Factory New)</strong> Giveaway!</a> <span class="glyphicon glyphicon-heart-empty"></span>';
-					}
-				}, 1000);
-			</script>
-		</div>
-		<div class="row">
 			<?php
 			if (!isset($_SESSION['UID'])){
 				require ('lib/view/coinflip.php');
